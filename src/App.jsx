01d0ff7e97cd711e45d478eee964a16d37bdadee
mehrdad Osouli Saran <mehrdadosoulisaran@gmail.com>
2023-12-04 23:31:13 +0330

    topbody set

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/header/Header'
+import TopBody from './components/TopBody/TopBody'
 
 function App() {
 
   return (
     <>
      <Header />
+     <TopBody />
     </>
   )
 }
