1e9d360010e0fc0afbce579fbb978867711faa4f
mehrdad <mehrdadosoulisaran@gmail.com>
2023-12-13 16:51:58 +0330

    edite section experiance

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -4,23 +4,22 @@ import Project_Item from "../project/Project_Item.jsx"
 // import image from '../../photos/image-1.jpg'
 const initial={
     galery:[
-        {name:'action',id:1,src:'../../photos/image-1.jpg'},
-        {name:'action',id:2,src:'../../photos/image2.jpg'},
-        {name:'comedy',id:3,src:'../../photos/image3.png'}, 
-        {name:'comedy',id:4,src:'../../photos/image4.png'},
-        {name:'comedy',id:5,src:'../../photos/image-1.jpg'},
-        {name:'detective',id:6,src:'../../photos/image2.jpg'},
-        {name:'detective',id:7,src:'../../photos/image4.png'},
+        {name:'Comedy',id:3,src:'../../photos/image3.png'}, 
+        {name:'Comedy',id:4,src:'../../photos/image4.png'},
+        {name:'Comedy',id:5,src:'../../photos/image-1.jpg'},
+        {name:'Detective',id:6,src:'../../photos/image2.jpg'},
+        {name:'Detective',id:7,src:'../../photos/image4.png'},
         {name:'Melodrama',id:8,src:'../../photos/image3.png'},
         {name:'Actionmovie',id:9,src:'../../photos/image3.png'},
-        {name:'HistoricalFilm',id:10,src:'../../photos/image3.png'},
-        {name:'Drema',id:11,src:'../../photos/image3.png'},
+        {name:'HistoricalFilm',id:10,src:'../../photos/image-1.jpg'},
+        {name:'Drema',id:11,src:'../../photos/image2.jpg'},
     ],
 update:[]}
 const actionElements=(state,name)=>{
-    state.filter(item=>{
+   const result= state.filter(item=>{
          return item.name == name
      })
+     return result
 }
 
 const reducer=(state,action)=>{
@@ -33,49 +32,40 @@ const reducer=(state,action)=>{
                 update:newState
             };
         case 'Melodrama':
-           const newState2=[...state.galery]
-           const result1= newState2.filter(item=>{
-                return item.name=='Melodrama'
-            })
-            actionElements(state.)
+            actionElements(state.galery,'Melodrama')
             return {
                 ...state,
-                update:[...result1]
+                update:actionElements(state.galery,'Melodrama')
             }
         case 'Comedy':
-            const newState3={...state}
-            const result2=newState3.galery.filter(item=>{return item.name =='comedy'})      
+            actionElements(state.galery,'Comedy')     
             return {
                 ...state,
-                update:[...result2]
+                update:actionElements(state.galery,'Comedy')
             };
         case 'Detective':
-            const newState4={...state}
-            const result3=newState4.galery.filter(item=>{return item.name =='detective'})      
+            actionElements(state.galery,'Detective')     
             return {
                 ...state,
-                update:[...result3]
+                update:actionElements(state.galery,'Detective')
             };
         case 'Actionmovie':
-            const newState5={...state}
-            const result4=newState5.galery.filter(item=>{return item.name =='Actionmovie'})      
+            actionElements(state.galery,'Actionmovie')     
             return {
                 ...state,
-                update:[...result4]
+                update:actionElements(state.galery,'Actionmovie')
             };
         case 'Drema':
-            const newState6={...state}
-            const result5=newState6.galery.filter(item=>{return item.name =='Drema'})      
+            actionElements(state.galery,'Drema')     
             return {
                 ...state,
-                update:[...result5]
+                update:actionElements(state.galery,'Drema')
             };
         case 'HistoricalFilm':
-            const newState7={...state}
-            const result6=newState7.galery.filter(item=>{return item.name =='HistoricalFilm'})      
+            actionElements(state.galery,'HistoricalFilm')      
             return {
                 ...state,
-                update:[...result6]
+                update:actionElements(state.galery,'HistoricalFilm')
             };
     
         default:
